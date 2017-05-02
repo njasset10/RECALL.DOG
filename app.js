7b7ff4cef6cfd412a83b2407ec43b0b98bf4fd86
njasset10 <njasset10@gmail.com>
2017-05-02 17:40:45 -0400

    Next steps outlined in comments

--- app.js
+++ app.js
@@ -43,6 +43,16 @@ function showscores(){
 	element.innerHTML = gameoverhtml;
 }
 
+// Need to create a back-end database which stores 4-option multiple choice questions/choices/answers with each unique URL (however many make sense)
+// on clicking the Chrome Extension - the user should be prompted with a "Do you want to remember this content?" and a "Recall.dog is on it" confirmation message
+// each user will utlimately have a list of URLs with varying ages since addition (ultimately they would have CRUD capability on this list) - the age of each URL governs which content users see on any day (managed by the Chrome "Alerts" function and a static schedule we've set by day)
+// each new day, the chrome plug-in displays a notification with the questions for that day (1 per URL if relevant to that day)
+// for each submitted response, users should see if they were correct or not, and see the correct answer)
+// when the User has completed all questions for the day, their score is shown as a % and this  % is stored 
+// users ultimately should have CRUD capability on all questions viewed (or content providers will provide)
+
+
+
 var questions = [
 	new Question("Who wrote this application?", ["Nick", "Hilary", "Parker", "Respy"],"Nick"),
 	new Question("What is Nick's dog's name?", ["Buddy", "Respy", "Dog", "Girl"],"Respy"),
