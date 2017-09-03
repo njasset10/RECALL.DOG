5740841e65a267a293dbab3ec448bb19fb80d82f
njasset10 <njasset10@gmail.com>
2017-09-03 16:34:59 -0400

    password protection

--- assets/js/website.js
+++ assets/js/website.js
@@ -1,1 +1,11 @@
 alert("hello")
+
+var password = prompt('What is the password?')
+
+if (password === "password") {
+  alert("yes!")
+} else {
+  while (password !="password") {
+  password = prompt("sorry, try again")
+  }
+}
