0a71015f3838bcdc23ed83f60dd99899817cfdfe
njasset10 <njasset10@gmail.com>
2017-09-04 22:47:07 -0400

    push

--- assets/js/scoreKeeper.js
+++ assets/js/scoreKeeper.js
@@ -4,22 +4,28 @@
 var button1 = document.querySelector("#p1")
 var button2 = document.querySelector("#p2")
 var buttonR = document.querySelector("#reset")
+var p1Display = document.querySelector("#p1Display")
+var p2Display = document.querySelector("#p2Display")
+
 var h1 = document.querySelector("h1")
+var h2 = document.querySelector("h2")
 var input = document.querySelector("input")
 
-input.addEventListener("change", )
+input.addEventListener("change", inputFunction)
 
 
+var winningScore = 5
 
-function functionName() {
-
+function inputFunction() {
+  winningScore = input.value
+  h2.textContent = "Playing to " + winningScore
 }
 
 button1.addEventListener("click", scoreChanger1)
 button2.addEventListener("click", scoreChanger2)
 buttonR.addEventListener("click", scoreR)
 
-var winningScore = 5
+
 var gameOver = false
 
 
@@ -28,36 +34,40 @@ var p1Score = 0
 var p2Score = 0
 
 function scoreChecker() {
-  if (p1Score >= winningScore || p2Score >= winningScore) {
+  if (p1Score >= winningScore) {
+    p1Display.classList.add("winner")
     return  gameOver = true
 }
+  if (p2Score >= winningScore) {
+    p2Display.classList.add("winner")
+    return  gameOver = true
+  }
 }
 
 function scoreChanger1() {
   if (!gameOver) {
     p1Score++
-    h1.textContent = p1Score + " to " + p2Score
+    p1Display.textContent = p1Score
     scoreChecker()
   }
 }
-
-
 
 
 function scoreChanger2() {
   if (!gameOver) {
     p2Score++
-    h1.textContent = p1Score + " to " + p2Score
+    p2Display.textContent = p2Score
     scoreChecker()
   }
 }
 
 
-
-
 function scoreR() {
 p1Score = 0
+    p1Display.textContent = p1Score
+    p1Display.classList.remove("winner")
 p2Score = 0
-  h1.textContent = p1Score + " to " + p2Score
-gameOver = false
+    p2Display.textContent = p2Score
+    p2Display.classList.remove("winner")
+return gameOver = false
 }
